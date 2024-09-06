8995f664f2c574e91c56b9ca97c74672fdad372b
jjuny-won <jjuny040627@gamil.com>
2024-09-06 14:14:29 +0900

    초기세팅

---
+++ src/main/java/com/readmate/ReadMate/ReadMateApplication.java
@@ -0,0 +1,13 @@
+package com.readmate.ReadMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readmate/ReadMate/ReadMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readmate.ReadMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
