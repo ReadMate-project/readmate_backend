3bd5cb05908e11152f240fc300c888e667500a82
inha0319@naver.com <inha031913@gmail.com>
2024-10-17 22:24:44 +0900

    [feat] 게시글 기능 수정 및 build.gradle 수정

--- src/main/java/com/readmate/ReadMate/config/S3Config.java
+++ src/main/java/com/readmate/ReadMate/config/S3Config.java
@@ -3,10 +3,10 @@ package com.readmate.ReadMate.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.services.s3.AmazonS3Client;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 
 @Configuration
