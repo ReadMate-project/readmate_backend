228b922e6969fe305a8a7a3232dc6a50fa184a7a
inha0319@naver.com <inha031913@gmail.com>
2024-10-30 16:48:14 +0900

    [fix] userRepo 어노테이션 추가

--- src/main/java/com/readmate/ReadMate/login/repository/UserRepository.java
+++ src/main/java/com/readmate/ReadMate/login/repository/UserRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
     Optional<User> findByUserId(Long userId);
