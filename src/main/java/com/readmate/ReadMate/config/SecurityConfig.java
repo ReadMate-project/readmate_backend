7244b517030637ac8514cc22ddc81137c0c12bf1
inha0319@naver.com <inha031913@gmail.com>
2024-11-06 11:19:47 +0900

    [feat] 배포된 주소로 프론트 cors 추가

--- src/main/java/com/readmate/ReadMate/config/SecurityConfig.java
+++ src/main/java/com/readmate/ReadMate/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 도메인
+        configuration.addAllowedOrigin("http://3.35.193.132:3000");
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용 (GET, POST, etc.)
         configuration.addAllowedHeader("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 인증 정보 허용 (쿠키 등)
